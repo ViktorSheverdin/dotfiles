7f8aa78a9620ed1ba6ab5b8184ee57192acabcc1
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2026-04-01 15:37:58 -0700

    create dotfile repo

---
+++ .config/nvim/lua/viktor/core/keymap.lua
@@ -0,0 +1,32 @@
+local keymap = vim.keymap -- for conciseness
+
+-- save
+keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
+keymap.set("i", "<C-s>", "<Esc><cmd>w<CR>", { desc = "Save file" })
+
+-- clear search highlights on Esc
+keymap.set("n", "<Esc>", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management (w = window)
+keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- buffer navigation (tab-like)
+keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+keymap.set("n", "<S-x>", function()
+	Snacks.bufdelete()
+end, { desc = "Close buffer" })
+
+-- tab management
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
